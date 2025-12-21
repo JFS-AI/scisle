b09f24a19d4b41724bfe23a29d5de76af31c3da2
Jan Franciszek Strzelecki <js480682@students.mimuw.edu.pl>
2025-12-21 17:59:22 +0100

    add kolejkakmax methods

--- prz.cpp
+++ prz.cpp
@@ -29,11 +29,39 @@ class kolejka_kmax {
 	queue<punkt> q;
 	deque<int> minima, maksima;
 
+public:
 	void wrzuc_przedzial_do_vectora() {
 		v.push_back({q.front(), q.back(), (q.back().x - q.front().x) / sqrt(q.size())});
 	}
+	void pop() {
+		int y = q.front().y;
+		if(y == minima.front())
+			minima.pop_front();
+		if(y == maksima.front())
+			maksima.pop_front();
+		q.pop();
+	}
+	bool is_bushable(punkt p) {
+		return p.y <= minima.front() + U && p.y >= maksima.front() - U;
+	}
 	void push(punkt p) {
-		return;
+		if(!is_pushable(p)) {
+			wrzuc_przedzial_do_vectora();
+			while(!is_pushable(p))
+				pop();
+		}
+
+		while(p.y < minima.back() && minima.size()) 
+			minima.pop_back();
+
+		minima.push_back(p.y);
+
+		while(p.y > maksima.back() && maksima.size()) 
+			maksima.pop_back();
+
+		maksima.push_back(p.y);
+
+		q.push(p);
 	}
 
 };
